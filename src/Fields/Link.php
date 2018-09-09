3df6fc21ce567822efb690260d17d153ef2212e1
Jordi Puigdellivol <jordi@gloobus.net>
2018-09-09 15:59:10 +0200

    added link field

---
+++ src/Fields/Link.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace BadChoice\Thrust\Fields;
+
+
+class Link extends Field
+{
+    public $showInEdit = false;
+    protected $link = '';
+    protected $classes = '';
+
+    public function link($link)
+    {
+        $this->link = $link;
+        return $this;
+    }
+
+    public function classes($classes)
+    {
+        $this->classes = $classes;
+        return $this;
+    }
+
+    public function displayInIndex($object)
+    {
+        return view('thrust::fields.link',[
+            'class' => $this->classes,
+            'value' => $this->getTitle(),
+            'url' => str_replace("{field}", $this->getValue($object) ,$this->link)
+        ]);
+    }
+
+    public function displayInEdit($object)
+    {
+        return $this->displayInIndex($object);
+    }
+
+
+}
