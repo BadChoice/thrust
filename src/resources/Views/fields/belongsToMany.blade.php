5c122aa73b62e2bea83ee394b3131018c43bcd17
Jordi Puigdellivol <jordi@gloobus.net>
2018-09-10 17:12:37 +0200

    Improved belognsto many

--- src/resources/Views/fields/belongsToMany.blade.php
+++ src/resources/Views/fields/belongsToMany.blade.php
@@ -1,6 +1,10 @@
 @if ($withLink)
     <a href="{{route('thrust.belongsToMany', [$resourceName, $id , $relationship])}}" class="showPopup">
-        {{ $value }}
+        @if( strlen($value) == 0)
+            --
+        @else
+            {{ $value }}
+        @endif
     </a>
 @else
     {{ $value }}
