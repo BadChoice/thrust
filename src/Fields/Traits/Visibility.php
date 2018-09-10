623e7d96ba8ca5cfe72af22bdc9599df74e37f51
Jordi Puigdellivol <jordi@gloobus.net>
2018-09-10 21:39:43 +0200

    added visiblitiy trait

---
+++ src/Fields/Traits/Visibility.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace BadChoice\Thrust\Fields\Traits;
+
+trait Visibility{
+
+    public $hideWhenField;
+    public $hideWhenValue;
+
+    public function hideWhen($field, $value = true)
+    {
+        $this->hideWhenField = $field;
+        $this->hideWhenValue = $value;
+        return $this;
+    }
+
+    public function shouldHide($object)
+    {
+        if ($this->hideWhenField == null) return false;
+        return $object->{$this->hideWhenField} === $this->hideWhenValue;
+    }
+
+}
