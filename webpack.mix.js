d738277b289cd3860c3c987db59bb1d485b26554
Jordi Puigdellivol <jordi@gloobus.net>
2018-09-22 16:30:40 +0200

    Fixes jquery-ui

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,7 +3,7 @@ let mix = require('laravel-mix');
 mix
     .babel([
         'src/resources/js/jquery-2.1.0.min.js',
-        'src/resources/js/jquery-ui-1.10.4.custom.min',
+        'src/resources/js/jquery-ui-1.10.4.custom.min.js',
         'src/resources/js/utils.js',
         'src/resources/js/searcher.js',
         'src/resources/js/visibility.js',
