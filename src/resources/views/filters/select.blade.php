41df87f719d0770b5c389745fb22e95c65e8f6bb
Jordi Puigdellívol <jordi@gloobus.net>
2018-10-23 16:11:28 +0200

    Fixes filterSelect

--- src/resources/views/filters/select.blade.php
+++ src/resources/views/filters/select.blade.php
@@ -1,6 +1,6 @@
 <select name="{{ $filter->class() }}" title="{{$filter->title()}}">
     <option value="--">--</option>
     @foreach ($filter->options() as $key => $optionValue)
-        <option value="{{$value}}" @if($value == $optionValue) selected @endif>{{$key}} </option>
+        <option value="{{$optionValue}}" @if($value != '--' && $value == $optionValue) selected @endif>{{$key}} </option>
     @endforeach
 </select>
