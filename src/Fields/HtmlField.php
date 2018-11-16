b65ecb09db55d99daa8e86b4fab80a2acd5086fc
Adria Martin <adria.martin@revo.works>
2018-11-16 10:39:21 +0100

    Adds HtmlField for Text Area with html tags

---
+++ src/Fields/HtmlField.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace BadChoice\Thrust\Fields;
+
+class HtmlField extends Field
+{
+    public $showInIndex = false;
+
+    public function displayInIndex($object)
+    {
+        return $this->getValue($object);
+    }
+
+    protected function getFieldAttributes()
+    {
+        return 'cols=50 rows=10';
+    }
+
+    public function displayInEdit($object, $inline = false)
+    {
+        return view('thrust::fields.textarea', [
+            'title'           => $this->getTitle(),
+            'field'           => $this->field,
+            'value'           => $this->getValue($object),
+            'validationRules' => $this->getHtmlValidation($object, 'textarea'),
+            'attributes'      => $this->getFieldAttributes()
+        ])->render();
+    }
+
+    public function getValue($object)
+    {
+        return $object->{$this->field};
+    }
+}
