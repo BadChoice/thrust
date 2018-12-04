0cda39f72f94762c5774eedfee2d8d93f65f5ec7
Adria Martin <adria.martin@revo.works>
2018-12-04 20:15:58 +0100

    Extends HtmlField to TextArea

--- src/Fields/HtmlField.php
+++ src/Fields/HtmlField.php
@@ -2,31 +2,8 @@
 
 namespace BadChoice\Thrust\Fields;
 
-class HtmlField extends Field
+class HtmlField extends TextArea
 {
-    public $showInIndex = false;
-
-    public function displayInIndex($object)
-    {
-        return $this->getValue($object);
-    }
-
-    protected function getFieldAttributes()
-    {
-        return 'cols=50 rows=10';
-    }
-
-    public function displayInEdit($object, $inline = false)
-    {
-        return view('thrust::fields.textarea', [
-            'title'           => $this->getTitle(),
-            'field'           => $this->field,
-            'value'           => $this->getValue($object),
-            'validationRules' => $this->getHtmlValidation($object, 'textarea'),
-            'attributes'      => $this->getFieldAttributes()
-        ])->render();
-    }
-
     public function getValue($object)
     {
         return $object->{$this->field};
