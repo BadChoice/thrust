abece20c92fa03c7bb439acd41467da585bac9d9
Jordi Puigdellívol <jordi@gloobus.net>
2018-12-24 13:49:58 +0100

    Fixes multiple select for old values + allowing non value selected

--- src/resources/views/fields/multipleSelect.blade.php
+++ src/resources/views/fields/multipleSelect.blade.php
@@ -1,7 +1,8 @@
 @component('thrust::components.formField', ["field" => $field, "title" => $title, "description" => $description ?? null, "inline" => $inline])
+    <input hidden name="{{$field}}" value="">
     <select id="{{$field}}" name="{{$field}}[]" @if($searchable) class="searchable" @endif multiple>
         @foreach($options as $key => $optionValue)
-            <option @if($value && in_array($key,$value)) selected @endif value="{{$key}}">{{$optionValue}}</option>
+            <option @if($value && in_array($key, (array)$value)) selected @endif value="{{$key}}">{{$optionValue}}</option>
         @endforeach
     </select>
 @endcomponent
