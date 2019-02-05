f02055b89b0d5c1d997e2150ed20df876299a4fb
paubenetprat <pau@revo.works>
2019-02-05 19:25:45 +0100

    add callback to currency display

--- src/Fields/Currency.php
+++ src/Fields/Currency.php
@@ -6,6 +6,9 @@ class Currency extends Decimal
 {
     public function displayInIndex($object)
     {
+        if ($this->displayInIndexCallback) {
+            return number_format(call_user_func($this->displayInIndexCallback, $object), 2) . ' €';
+        }
         return number_format($this->getValue($object), 2) . ' €';
     }
 }
