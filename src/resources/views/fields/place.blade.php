1673aeb89bfeda657ea8951b29ecc194bbb428ec
paubenetprat <pau@revo.works>
2019-02-20 10:06:57 +0100

    add validation rules on address field

--- src/resources/views/fields/place.blade.php
+++ src/resources/views/fields/place.blade.php
@@ -1,6 +1,6 @@
 @component('thrust::components.formField', ["field" => $field, "title" => $title, "description" => $description ?? null])
     <div class="places" style="width:300px">
-        <input type="text" name="{{$field}}" id="{{$field}}" placeholder="{{$title}}" value="{{$value}}"
+        <input type="text" name="{{$field}}" id="{{$field}}" placeholder="{{$title}}" value="{{$value}}"  {!! $validationRules !!} 
         style="width:300px; border: 1px solid #c8c8c8; color: #32393F; height: 26px; border-radius: 4px; padding-left: 12px; font-size:10px" autocomplete="off"/>
     </div>
 
