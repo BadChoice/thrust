bd65369ca94ca0374c5161af8f1bb3eea2ce06ab
Jordi Puigdellívol <jordi@gloobus.net>
2019-03-19 15:34:18 +0100

    Added table density class so it can be hidden

--- src/resources/views/components/tableDensity.blade.php
+++ src/resources/views/components/tableDensity.blade.php
@@ -1,3 +1,5 @@
-<a id='collapse' class="mr1 pointer o30"    onclick="$('table').removeClass('expanded').addClass('condensed');    $(this).removeClass('o30'); $('#expand').addClass('o30'); $('#expand').addClass('o30');">@icon(bars)</a>
-<a id='normal'   class="mr1 pointer"        onclick="$('table').removeClass('condensed expanded'); $(this).removeClass('o30'); $('#collapse').addClass('o30'); $('#expand').addClass('o30');">@icon(list)</a>
-<a id='expand'   class="pointer o30"    onclick="$('table').removeClass('condensed').addClass('expanded'); $(this).removeClass('o30'); $('#collapse').addClass('o30'); $('#normal').addClass('o30');">@icon(th-list)</a>
+<div class="thrust-table-density">
+    <a id='collapse' class="mr1 pointer o30"    onclick="$('table').removeClass('expanded').addClass('condensed');    $(this).removeClass('o30'); $('#expand').addClass('o30'); $('#expand').addClass('o30');">@icon(bars)</a>
+    <a id='normal'   class="mr1 pointer"        onclick="$('table').removeClass('condensed expanded'); $(this).removeClass('o30'); $('#collapse').addClass('o30'); $('#expand').addClass('o30');">@icon(list)</a>
+    <a id='expand'   class="pointer o30"    onclick="$('table').removeClass('condensed').addClass('expanded'); $(this).removeClass('o30'); $('#collapse').addClass('o30'); $('#normal').addClass('o30');">@icon(th-list)</a>
+</div>
