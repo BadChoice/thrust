cdfd97aca749c4c7d9a7d193945e5fbaff1d0a6b
Pau Benet Prat <pau@revo.works>
2019-03-27 16:15:32 +0100

    add invert option on visibility trait

--- src/Fields/Traits/Visibility.php
+++ src/Fields/Traits/Visibility.php
@@ -6,11 +6,21 @@ trait Visibility
 {
     public $hideWhenField;
     public $hideWhenValue;
+    public $showWhenField;
+    public $showWhenValue;
+    public $invert;
 
     public function hideWhen($field, $value = true)
     {
-        $this->hideWhenField = $field;
-        $this->hideWhenValue = $value;
+        $this->hideWhenField    = $field;
+        $this->hideWhenValue    = $value;
+        return $this;
+    }
+
+    public function showWhen($field, $value = true)
+    {
+        $this->showWhenField    = $field;
+        $this->showWhenValue    = $value;
         return $this;
     }
 
@@ -19,6 +29,14 @@ trait Visibility
         if ($this->hideWhenField == null) {
             return false;
         }
-        return $object->{$this->hideWhenField} === $this->hideWhenValue;
+        return $object->{$this->hideWhenField} !== $this->hideWhenValue;
+    }
+
+    public function shouldShow($object)
+    {
+        if ($this->showWhenField == null) {
+            return true;
+        }
+        return $object->{$this->showWhenField} === $this->showWhenValue;
     }
 }
