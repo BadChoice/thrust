25d9b6d716c96e9d114999d623df5b2c1da5c8b4
Jordi Puigdellívol <jordi@gloobus.net>
2019-04-09 17:56:48 +0200

    :zap: Partition metric can work with non relationships now

--- src/Metrics/PartitionMetric.php
+++ src/Metrics/PartitionMetric.php
@@ -6,7 +6,10 @@ use Illuminate\Support\Facades\DB;
 
 abstract class PartitionMetric extends Metric
 {
-    private $relationKey;
+    private $field;
+    private $foreign_key;
+    private $namesCallback;
+    private $colorsCallback;
 
     public function metricTypeName()
     {
@@ -16,33 +19,76 @@ abstract class PartitionMetric extends Metric
     public function getColors()
     {
         return $this->result->map(function($value){
-            if (!$value->{$this->relationKey}){
+            if ($this->colorsCallback){
+                return call_user_func($this->colorsCallback, $value);
+            }
+            if (! $value->{$this->field}){
                 return "#eee";
             }
-            return $value->{$this->relationKey}->color ?? $this->colorFromName($value->{$this->relationKey}->name);
+            if ($this->foreign_key) {
+                return $value->{$this->field}->color ?? $this->colorFromName($value->{$this->field}->name);
+            }
+            return $this->colorFromName($value->{$this->field});
         })->values();
     }
 
     protected function result()
     {
         return $this->result->mapWithKeys(function($value){
-            return [($value->{$this->relationKey}->name ?? '--') . " ({$value->count})"=> $value->count];
+            if ($this->namesCallback){
+                return [call_user_func($this->namesCallback, $value) . " ({$value->count})" => $value->count];
+            }
+            if ($this->foreign_key) {
+                return [($value->{$this->field}->name ?? '--') . " ({$value->count})" => $value->count];
+            }
+            return [($value->{$this->field} ?? '--') . " ({$value->count})" => $value->count];
         });
     }
 
     public function count($class, $field)
     {
-        $foreign_key = (new $class)->$field()->getForeignKey();
-        $this->relationKey = $field;
-        $this->result = $this->applyRange($class)->with($field)
-                      ->groupBy($foreign_key)
+        $queryField = $this->setField($class, $field);
+        $this->result = $this->withRelationShip($this->applyRange($class))
+                      ->groupBy($queryField)
                       ->orderBy('count','DESC')
-                      ->select($foreign_key, DB::raw("$foreign_key as field"), DB::raw("count(id) as count"))->get();
+                      ->select($queryField, DB::raw("$queryField as field"), DB::raw("count(id) as count"))->get();
         return $this;
     }
 
     public function colorFromName($text)
     {
         return "#".substr(md5($text), 0, 6);
+    }
+
+    private function setField($class, $field)
+    {
+        $foreign_key = null;
+        $this->field = $field;
+        try {
+            $this->foreign_key = (new $class)->$field()->getForeignKey();
+        }catch(\Exception $e){
+
+        }
+        return $this->getQueryField();
+    }
+    protected function getQueryField(){
+        if ($this->foreign_key) return $this->foreign_key;
+        return $this->field;
+    }
+
+    private function withRelationShip(\Illuminate\Database\Eloquent\Builder $query)
+    {
+        if (! $this->foreign_key) return $query;
+        return $query->with($this->field);
+    }
+
+    public function names($callback)
+    {
+        $this->namesCallback = $callback;
+    }
+
+    public function colors($callback)
+    {
+        $this->colorsCallback = $callback;
     }
 }
