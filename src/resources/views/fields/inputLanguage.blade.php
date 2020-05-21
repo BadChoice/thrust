58dc17e8160eb76346fe7ef4f6f2a7635158cf11
Jordi Puigdellívol <jordi@gloobus.net>
2020-05-21 17:23:58 +0200

    Added TextLanguage field

---
+++ src/resources/views/fields/inputLanguage.blade.php
@@ -0,0 +1,25 @@
+@component('thrust::components.formField' , ["field" => $field, "title" => $title, "description" => $description, "inline" => $inline])
+    @php $id = str_replace("]", "", str_replace("[", "", $field)) @endphp
+    <div class="text-right mt2 mb3" style="max-width:300px">
+        @foreach ($languages as $language)
+            <a class="secondary p2 pointer uppercase circle languageSelector{{$id}}" id="{{$id}}languageSelector{{$language}}" onclick="changeLanguage('{{$id}}','{{$language}}')">{{ $language }}</a>
+        @endforeach
+    </div>
+
+    @foreach ($languages as $language)
+        <input type={{$type}} id="{{$id}}{{$language}}" value="{{$value[$language] ?? ""}}" name="{{$field}}[{{$language}}]" placeholder="{{$title}}"
+               {{$attributes}} {!! $validationRules !!} @if($inline) style="width:auto;" @endif class="hidden languageField{{$id}}">
+    @endforeach
+
+    @push('edit-scripts')
+        <script>
+            changeLanguage("{{$id}}", "{{$languages[0]}}");
+            function changeLanguage(field, language) {
+                $('.languageSelector'+field).css("background-color","#fff");
+                $('#'+field+'languageSelector'+language).css("background-color","#eee");
+                $('.languageField' + field).hide();
+                $('#'+field+language).removeClass('hidden').show();
+            }
+        </script>
+    @endpush
+@endcomponent
