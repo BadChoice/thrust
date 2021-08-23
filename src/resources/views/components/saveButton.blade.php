eb2c5b949b494e8f698903fe3d50cb4c2b153c6a
aleixgilaguilar <aleix.gil@revo.works>
2021-08-23 10:22:17 +0200

    Multiline option for confirm translations.

--- src/resources/views/components/saveButton.blade.php
+++ src/resources/views/components/saveButton.blade.php
@@ -1,4 +1,4 @@
-<button class="button-with-loading" @if($updateConfirmationMessage) onclick="return confirm('{{$updateConfirmationMessage}}')" @endif>
+<button class="button-with-loading" @if($updateConfirmationMessage) onclick="return confirm(`{{$updateConfirmationMessage}}`)" @endif>
         <span class="loadingImage">
             <i class="fa fa-circle-o-notch fa-spin"></i>
         </span>
