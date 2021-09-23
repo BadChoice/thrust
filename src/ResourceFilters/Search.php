87bdb2abdf52f38b3a72c42444741b31f05afc6b
aleixgilaguilar <aleix.gil@revo.works>
2021-09-23 11:00:37 +0200

    Apply search fields, query fixed.

--- src/ResourceFilters/Search.php
+++ src/ResourceFilters/Search.php
@@ -9,13 +9,12 @@ class Search
     public static function apply($query, $searchText, $searchFields)
     {
         $searchFields = collect($searchFields);
-        $firstField   = $searchFields->shift();
-        static::applyField($query, $firstField, $searchText);
-
-        return $query->orWhere(function ($query) use($searchText, $searchFields){
-            return $searchFields->reduce(function($query, $searchField) use($searchText) {
-                return static::applyFieldSimple($query, $searchField, $searchText);
-            }, $query);
+        return $query->where(function ($query) use($searchText, $searchFields){
+            return $query->orWhere(function ($query) use($searchText, $searchFields) {
+                return $searchFields->reduce(function ($query, $searchField) use ($searchText) {
+                    return static::applyFieldSimple($query, $searchField, $searchText);
+                }, $query);
+            });
         })->distinct();
     }
 
