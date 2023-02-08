ca7e154e0d77b59cb37f6849202e1a0e13beda16
Jordi Puigdellívol <jordi.p@revo.works>
2023-02-08 12:32:32 +0100

    Import action commented

--- src/Resource.php
+++ src/Resource.php
@@ -262,7 +262,7 @@ abstract class Resource
     public function mainActions()
     {
         return  [
-            ...(static::$importable ? [new Import()] : []),
+            //...(static::$importable ? [new Import()] : []),
             MainAction::make('new'),
         ];
     }
