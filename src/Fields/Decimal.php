579c2ae68be36eb0a6cea472b680333ba914b68e
Alex Deniz Garcia <alex.deniz@revo.works>
2023-08-29 13:19:04 +0200

    REV-12275: make decimal field nullable

--- src/Fields/Decimal.php
+++ src/Fields/Decimal.php
@@ -5,11 +5,15 @@ namespace BadChoice\Thrust\Fields;
 class Decimal extends Text
 {
     protected $asInteger = false;
+    protected $nullable = false;
     public $rowClass = 'text-right';
 
     public function getValue($object)
     {
         $value = parent::getValue($object);
+        if ($value === null & $this->nullable) {
+            return null;
+        }
         if ($value && $this->asInteger) {
             return number_format(floatval($value) / 100.0, 2);
         }
@@ -27,6 +31,12 @@ class Decimal extends Text
     public function asInteger($asInteger = true)
     {
         $this->asInteger = $asInteger;
+        return $this;
+    }
+
+    public function nullable($nullable = true)
+    {
+        $this->nullable = $nullable;
         return $this;
     }
 
